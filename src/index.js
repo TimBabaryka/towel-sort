d8d8157578c2cac843e778e291ed5227283a7deb
Tim Babaryka <timasky95@gmail.com>
2021-08-29 20:31:20 +0300

    feat: js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,34 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (matrix === undefined || matrix.length === 0 ) {
+    return []
+  }
+  for( let i=1; i<matrix.length; i++){
+     matrix[i++].reverse();
+  }
+    return matrix.flat()
+  }
+
+
+
+
+
+
+
+
+
+
+// if (Array.length > 9) {
+//   let decision = matrix[1,1].reverse()+ matrix[3,3].reverse()
+//   decision=matrix.toString()
+//    decision = decision.split(',')
+//   return decision.map(Number);
+//   } else {
+//     let decision = matrix[1,1].reverse()
+//     decision=matrix.toString()
+//      decision = decision.split(',')
+//     return decision.map(Number);
+
+
+
